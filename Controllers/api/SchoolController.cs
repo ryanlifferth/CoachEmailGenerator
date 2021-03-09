0726267f3aaa3daa5c89d07f1eedb357d367e2f2
Ryan Lifferth <ryanlifferth@gmail.com>
2021-03-08 21:22:51 -0700

    New controllers and styling

---
+++ Controllers/api/SchoolController.cs
@@ -0,0 +1,63 @@
+﻿using CoachEmailGenerator.Models;
+using CoachEmailGenerator.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CoachEmailGenerator.Controllers.api
+{
+    public class SchoolController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        private SaveTemplateService _saveTemplateService;
+
+        public SchoolController(ILogger<HomeController> logger,
+            SaveTemplateService saveTemplateService)
+        {
+            _logger = logger;
+            _saveTemplateService = saveTemplateService;
+        }
+
+        public IActionResult Index()
+        {
+            var fileName = _saveTemplateService.GetUserNameFromEmail(User.Claims.FirstOrDefault(x => x.Type.ToString().IndexOf("emailaddress") > 0)?.Value);
+            var filePath = Directory.GetCurrentDirectory() + "\\Data\\" + fileName + "-schools.json";
+
+            var jsonString = System.IO.File.Exists(filePath) ? System.IO.File.ReadAllText(filePath) : string.Empty;
+            var school = !string.IsNullOrEmpty(jsonString) ? JsonSerializer.Deserialize<List<School>>(jsonString) : null;
+
+            return View(school);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index([Bind("SchoolName, SchoolNameShort, HeadCoach.Name, HeadCoach.Email, HeadCoach.PhoneNumber, IsEnabled")] EmailTemplate template)
+        {
+
+            /************ TEMP CODE **************/
+            var schools = new List<School>()
+            {
+                new School { SchoolName = "Brigham Young University", SchoolNameShort = "BYU", HeadCoach = new Coach { Name = "Coach Smith", Email = "coach@byu.edu", PhoneNumber = "801-555-1234" }, IsEnabled = true },
+                new School { SchoolName = "Utah Valley University", SchoolNameShort = "UVU", HeadCoach = new Coach { Name = "Greg Maas", Email = "greg.maas@uvu.edu" }, IsEnabled = true },
+                new School { SchoolName = "Seattle University", SchoolNameShort = "SU", HeadCoach = new Coach { Name = "Pete Fewing", Email = "fewingp@seattleu.edu", PhoneNumber = "206-296-5498" }, IsEnabled = true }
+            };
+            
+            var fileName = _saveTemplateService.GetUserNameFromEmail(User.Claims.FirstOrDefault(x => x.Type.ToString().IndexOf("emailaddress") > 0)?.Value);
+            var filePath = Directory.GetCurrentDirectory() + "\\Data\\" + fileName + "-schools.json";
+
+            var jsonString = JsonSerializer.Serialize(schools);
+            System.IO.File.WriteAllText(filePath, jsonString);
+            /********** END TEMP CODE ************/
+
+
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/api/TemplateController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoachEmailGenerator.Controllers.api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TemplateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
